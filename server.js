c3a0ae89a8bb5f955b1d068ee719761ae5aa6020
Kevin Old <kevin@kevinold.com>
2014-12-06 20:25:42 -0600

    statically serve dist directory via hapi

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server(3000);
 
+// Statically serve 'dist' directory
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'dist'
+        }
+    }
+});
+
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
